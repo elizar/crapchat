307a6eacc2ff475caaaf31e68219f2fd7369bb97
Elizar Pepino <jupenz@gmail.com>
2013-08-26 02:44:38 +0800

    prettify them codes :P~

--- ws.js
+++ ws.js
@@ -2,72 +2,74 @@ var net = require('net');
 var wsp = require('./wsp');
 var sockets = [];
 var server = net.createServer(function(socket) {
-			if (sockets.indexOf(socket) === -1) {
-				socket.nick = Date.now().toString();
-				sockets.push(socket);
-			}
+    if (sockets.indexOf(socket) === -1) {
+        socket.nick = Date.now().toString();
+        sockets.push(socket);
+    }
 
-			socket.on('end', function(s) {
-				console.log(sockets.indexOf(socket));
-				var deleted = sockets.splice(sockets.indexOf(socket), 1);
-				if (deleted.length > 0) {
-					sockets.forEach(function(s) {
-						s.write(new Buffer(wsp.encode('[-Server Notice-] ' + socket.nick + ' disconnected from server')));
-					});
-					console.log('Socket deleted');
-				}
-			});
+    socket.on('end', function(s) {
+        console.log(sockets.indexOf(socket));
+        var deleted = sockets.splice(sockets.indexOf(socket), 1);
+        if (deleted.length > 0) {
+            sockets.forEach(function(s) {
+                s.write(new Buffer(wsp.encode('[-Server Notice-] ' + socket.nick + ' disconnected from server')));
+            });
+            console.log('Socket deleted');
+        }
+    });
 
-			socket.on('error', console.log);
+    socket.on('error', console.log);
 
-			socket.on('data', function(d) {
-				if (d.toString().indexOf('Sec-WebSocket-Key') === -1) {
-					if (d[0] === 136) {
-						// if this is close frame exit this shit
-						socket.end();
-						return;
-					}
-					var txt = wsp.parse(d);
-					var isNickChanged = false,
-							oldNick = socket.nick;
-					if (txt.indexOf('/nick ') !== -1) {
-						// if change of nick is present
-						var at = txt.split(' ');
-						socket.nick = at[1];
-						socket.write(new Buffer(wsp.encode('You are now known as '+at[1])));
-						isNickChanged = true;
-					}
-					sockets.forEach(function(s) {
-						if (s.writable) {
-							if (isNickChanged) {
-								if (s !== socket)
-								s.write(new Buffer(wsp.encode('[-Server Notice-] ' + oldNick + ' is now known as ' + socket.nick)));
-							}
-							else
-							s.write(new Buffer(wsp.encode(socket.nick + ' says: ' + txt)));
-						} else {
-							// end socket if it's not writable
-							// this is a fix of chrome's not sending close frame issue
-							s.end();
-						}
-					});
-					return;
-				}
-				// Handle handshake from client
-				var response = wsp.handshake(d);
-				this.write(response);
+    socket.on('data', function(d) {
+        if (d.toString().indexOf('Sec-WebSocket-Key') === -1) {
+            if (d[0] === 136) {
+                // if this is close frame exit this shit
+                socket.end();
+                return;
+            }
+            var txt = wsp.parse(d);
+            var isNickChanged = false,
+                oldNick = socket.nick;
+            if (txt.indexOf('/nick ') !== -1) {
+                // if change of nick is present
+                var at = txt.split(' ');
+                socket.nick = at[1];
+                socket.write(new Buffer(wsp.encode('You are now known as ' + at[1])));
+                isNickChanged = true;
+            }
+            sockets.forEach(function(s) {
+                if (s.writable) {
+                    if (isNickChanged) {
+                        if (s !== socket)
+                            s.write(new Buffer(wsp.encode('[-Server Notice-] ' + oldNick + ' is now known as ' + socket.nick)));
+                    } else
+                        s.write(new Buffer(wsp.encode(socket.nick + ' says: ' + txt)));
+                } else {
+                    // end socket if it's not writable
+                    // this is a fix of chrome's not sending close frame issue
+                    s.end();
+                }
+            });
+            return;
+        }
+        // Handle handshake from client
+        var response = wsp.handshake(d);
+        this.write(response);
 
-			});
+    });
 
-		});
-		server.listen(1337, function() {
-			console.log('Server running on port 3000');
-		});
+});
+server.listen(1337, function() {
+    console.log('Server running on port 3000');
+});
 
 // our webserver
 var webserver = require('http').createServer(web);
-		webserver.listen(3000);
+webserver.listen(3000);
+
 function web(req, res) {
-	res.writeHead(200, {'Content-Type':'text/html'});
-	res.end(require('fs').readFileSync('./chat.html'));
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(require('fs').readFileSync('./chat.html'));
 }
--- wsp.js
+++ wsp.js
@@ -1,66 +1,66 @@
 var crypto = require('crypto');
 
 exports.parse = function(d) {
-	var datalength = d[1] & 127;
-	var indexFirstMask = 2;
-	if (datalength == 126) {
-		indexFirstMask = 4;
-	} else if (datalength == 127) {
-		indexFirstMask = 10;
-	}
-	var masks = d.slice(indexFirstMask,indexFirstMask + 4);
-	var i = indexFirstMask + 4;
-	var index = 0;
-	var output = "";
-	while (i < d.length) {
-		output += String.fromCharCode(d[i++] ^ masks[index++ % 4]);
-	}
-	return output;
+    var datalength = d[1] & 127;
+    var indexFirstMask = 2;
+    if (datalength == 126) {
+        indexFirstMask = 4;
+    } else if (datalength == 127) {
+        indexFirstMask = 10;
+    }
+    var masks = d.slice(indexFirstMask, indexFirstMask + 4);
+    var i = indexFirstMask + 4;
+    var index = 0;
+    var output = "";
+    while (i < d.length) {
+        output += String.fromCharCode(d[i++] ^ masks[index++ % 4]);
+    }
+    return output;
 };
 
 exports.encode = function(d) {
-	var bytesFormatted = [];
-	bytesFormatted[0] = 129;
-	if (d.length <= 125) {
-		bytesFormatted[1] = d.length;
-	} else if (d.length >= 126 && d.length <= 65535) {
-		bytesFormatted[1] = 126;
-		bytesFormatted[2] = ( d.length >> 8 ) & 255;
-		bytesFormatted[3] = ( d.length      ) & 255;
-	} else {
-		bytesFormatted[1] = 127;
-		bytesFormatted[2] = ( d.length >> 56 ) & 255;
-		bytesFormatted[3] = ( d.length >> 48 ) & 255;
-		bytesFormatted[4] = ( d.length >> 40 ) & 255;
-		bytesFormatted[5] = ( d.length >> 32 ) & 255;
-		bytesFormatted[6] = ( d.length >> 24 ) & 255;
-		bytesFormatted[7] = ( d.length >> 16 ) & 255;
-		bytesFormatted[8] = ( d.length >>  8 ) & 255;
-		bytesFormatted[9] = ( d.length       ) & 255;
-	}
-	for (var i = 0; i < d.length; i++){
-		bytesFormatted.push(d.charCodeAt(i));
-	}
-	return bytesFormatted;
+    var bytesFormatted = [];
+    bytesFormatted[0] = 129;
+    if (d.length <= 125) {
+        bytesFormatted[1] = d.length;
+    } else if (d.length >= 126 && d.length <= 65535) {
+        bytesFormatted[1] = 126;
+        bytesFormatted[2] = (d.length >> 8) & 255;
+        bytesFormatted[3] = (d.length) & 255;
+    } else {
+        bytesFormatted[1] = 127;
+        bytesFormatted[2] = (d.length >> 56) & 255;
+        bytesFormatted[3] = (d.length >> 48) & 255;
+        bytesFormatted[4] = (d.length >> 40) & 255;
+        bytesFormatted[5] = (d.length >> 32) & 255;
+        bytesFormatted[6] = (d.length >> 24) & 255;
+        bytesFormatted[7] = (d.length >> 16) & 255;
+        bytesFormatted[8] = (d.length >> 8) & 255;
+        bytesFormatted[9] = (d.length) & 255;
+    }
+    for (var i = 0; i < d.length; i++) {
+        bytesFormatted.push(d.charCodeAt(i));
+    }
+    return bytesFormatted;
 };
 
 exports.handshake = function(d) {
-	var headers = d.toString().split('\r\n'),
-			parsedHeaders = {};
-	headers.forEach(function(text) {
-		if (text !== 'GET / HTTP/1.1' && text !== '') {
-			var textArray = text.split(':');
-			parsedHeaders[textArray[0]] = textArray[1].trim();
-		}
-	});
-	var swa = crypto.createHash('sha1')
-						.update(parsedHeaders['Sec-WebSocket-Key'] + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
-						.digest('base64');
-	var response	= 'HTTP/1.1 101 Switching Protocols\r\n';
-			response += 'Upgrade: websocket\r\n';
-			response += 'Connection: Upgrade\r\n';
-			response += 'Sec-WebSocket-Accept: '+swa+'\r\n';
-			response += 'Sec-WebSocket-Protocol: chat\r\n';
-			response += '\r\n';
-	return response;
+    var headers = d.toString().split('\r\n'),
+        parsedHeaders = {};
+    headers.forEach(function(text) {
+        if (text !== 'GET / HTTP/1.1' && text !== '') {
+            var textArray = text.split(':');
+            parsedHeaders[textArray[0]] = textArray[1].trim();
+        }
+    });
+    var swa = crypto.createHash('sha1')
+        .update(parsedHeaders['Sec-WebSocket-Key'] + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
+        .digest('base64');
+    var response = 'HTTP/1.1 101 Switching Protocols\r\n';
+    response += 'Upgrade: websocket\r\n';
+    response += 'Connection: Upgrade\r\n';
+    response += 'Sec-WebSocket-Accept: ' + swa + '\r\n';
+    response += 'Sec-WebSocket-Protocol: chat\r\n';
+    response += '\r\n';
+    return response;
 };
